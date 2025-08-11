fe7bf06fe417c52fcf8fc57da6906e94c21976a6
tra <tralegendary99@gmail.com>
2025-08-11 14:43:14 +0700

    update

---
+++ Next-Gen-Javascript-Solved/Teaching/Week1/Material/EX-5/main.js
@@ -0,0 +1,32 @@
+// Demo EX-5: C to JavaScript Conversion - Logical Operators
+// Original C Code:
+/*
+#include <stdio.h>
+
+int main() {
+    int isLoggedIn = 1; // true
+    int isAdmin = 0;    // false
+
+    if (isLoggedIn && isAdmin) {
+        printf("Access granted\n");
+    } else {
+        printf("Access denied\n");
+    }
+
+    if (!isAdmin) {
+        printf("Not an admin\n");
+    }
+
+    return 0;
+}
+*/
+
+// JavaScript Implementation:
+
+let isLoggedIn = 1;
+let isAdmin = 0;
+
+if (isLoggedIn && isAdmin) console.log("Access granted\n");
+else console.log("Access denied\n");
+
+if (!isAdmin) console.log("Not an admin\n");
